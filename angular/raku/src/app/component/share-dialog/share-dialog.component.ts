a1910373c5b51b63c76afba29a4fcdf102332f55
dingnjat <annanshakai@gmail.com>
2020-12-08 20:50:54 +0700

    share

--- angular/raku/src/app/component/share-dialog/share-dialog.component.ts
+++ angular/raku/src/app/component/share-dialog/share-dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import * as moment from "moment";
+import {AppSettings} from "../../global/app-settings";
 
 @Component({
   selector: 'app-share-dialog',
@@ -9,15 +10,22 @@ import * as moment from "moment";
 })
 export class ShareDialogComponent implements OnInit {
 
+  shareLink: string;
+
   constructor(public dialogRef: MatDialogRef<ShareDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,) {
   }
 
   ngOnInit(): void {
+    this.shareLink = (AppSettings.BASE_URL + this.data.track.link).trim();
     console.log(this.data.track);
   }
 
   convertTimespan(time: Date) {
     return moment(time).startOf("second").fromNow();
+  }
+
+  encodeUrl(url) {
+    return encodeURIComponent(url);
   }
 }
