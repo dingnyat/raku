7798be17d9c6fb7e7e56a533af66262717fea883
dingnjat <annanshakai@gmail.com>
2020-12-20 00:22:16 +0700

    change port, deploy to server

--- angular/raku/src/app/global/app-settings.ts
+++ angular/raku/src/app/global/app-settings.ts
@@ -1,9 +1,9 @@
 export class AppSettings {
-  public static BASE_URL = "http://localhost:4201";
+  public static BASE_URL = "http://localhost:6789";
   public static COOKIE_TOKEN_NAME = "RakuToken";
   public static COOKIE_TOKEN_EXPIRE_TIME = 864000000; // second
-  public static ENDPOINT = "http://localhost:8081";
-  public static OAUTH2_REDIRECT_URI = "http://localhost:4201/oauth2/redirect";
+  public static ENDPOINT = "http://localhost:9876";
+  public static OAUTH2_REDIRECT_URI = "http://localhost:6789/oauth2/redirect";
   public static GOOGLE_AUTH_URL = encodeURI(AppSettings.ENDPOINT + "/oauth2/authorize/google?redirect_uri=" + AppSettings.OAUTH2_REDIRECT_URI);
   public static FACEBOOK_AUTH_URL = encodeURI(AppSettings.ENDPOINT + '/oauth2/authorize/facebook?redirect_uri=' + AppSettings.OAUTH2_REDIRECT_URI);
 }
