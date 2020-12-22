330f8afbe186daeee48a05c69273d92c1755b75a
dingnyat <annanshakai@gmail.com>
2020-12-22 16:46:17 +0700

    fix bugs

--- angular/raku/src/app/component/user-security/user-security.component.ts
+++ angular/raku/src/app/component/user-security/user-security.component.ts
@@ -2,7 +2,6 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {UserService} from "../../service/user.service";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {faTimes} from "@fortawesome/free-solid-svg-icons";
-import {User} from "../../model/user";
 import {AppSettings} from "../../global/app-settings";
 
 @Component({
@@ -12,7 +11,7 @@ import {AppSettings} from "../../global/app-settings";
 })
 export class UserSecurityComponent implements OnInit {
   faTimes = faTimes;
-  user: User;
+  user: any = {};
   link = AppSettings.BASE_URL + "/";
 
   usernameExisted = false;
